ba121bb097d68857666368d206707da65ed86418
Ursule <ursuledabo1@gmail.com>
2022-08-28 21:16:51 +0300

    signup update

--- controllers/user.js
+++ controllers/user.js
@@ -8,9 +8,18 @@ exports.signup =  async(req,res)=>{
         .then(
           userHashedPassword =>{ 
               const user = new User({     
-                  id_User : new mongoose.Types.ObjectId(),
-                  userMail : req.body.userMail ,
-                  userPassword: userHashedPassword,      
+                  id_User : new mongoose.Types.ObjectId(),  
+                  userFirstName: req.body.userFirstName ,
+                  userLastName : req.body.userLastName ,
+                  userBirthdate: req.body.userBirthdate , 
+                  userGender : req.body.userGender , 
+                  userAddress : req.body.userAddress ,
+                  userContact : req.body.userContact ,
+                  userMail : req.body.userMail,
+                  userPassword: userHashedPassword ,
+                  userCin: req.body.userCin,
+                  userProfession: req.body.userMail ,
+                  userPicture: req.body.userPicture ,   
               });
               user.save()
               .then(() => res.status(201).json({ message: 'USER CREATED SUCCESSFULLY !' }))
@@ -19,3 +28,30 @@ exports.signup =  async(req,res)=>{
         .catch(error => res.status(500).json({ error }));
   
   }
+  
+exports.login = (req,res,next) => {
+    User.findOne({userMail:req.body.userMail})
+    .then(user => {
+        if (!user){
+            return res.status(401).json({message: "User not found"})
+        }
+        bcrypt.compare(req.body.userPassword,user.userPassword)
+        .then(valid => {
+            if (!valid){
+                return res.status(401).json({error:"Incorret password" })
+            }
+            res.status(200).json({
+               
+                userId: user._id,
+                token : jwt.sign(
+                    {userId: user._id},
+                    'ISPM_TRES_SUCRE_2022',
+                    {expiresIn: '24h'}
+                )
+                
+            });
+        })
+        .catch(error => res.status(500).json({error}));
+    })
+    .catch(error => res.status(500).json({error}));
+}
--- models/User.js
+++ models/User.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-   /* userFirstName: {type:String,required:true},
+    userFirstName: {type:String,required:true},
     userLastName : {type:String,required:true},
     userBirthdate: {type:String,required:true}, 
     userGender : {type:String,required:true}, 
     userAddress : {type:String,required:true},
-    userContact : {type:String,required:true},*/
+    userContact : {type:String,required:true},
     userMail : {type:String,required:true},
-    userPassword: {type:String,required:true}
-    /*userCin: {type:Number,required:true},
+    userPassword: {type:String,required:true},
+    userCin: {type:Number,required:true},
     userProfession: {type:String,required:true},
-    userPicture: {type:String,required:true},*/
+    userPicture: {type:String,required:true},
 });
 
 module.exports = mongoose.model('User',userSchema);
